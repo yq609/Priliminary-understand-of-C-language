01900e3202e67e4d29b8c8863456c7b2bfe7f5c0
yq609 <1582746393@qq.com>
2024-10-03 14:28:52 +0800

    Add files via upload

---
+++ plane.c
@@ -0,0 +1,15 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+// 
+//int main()
+//{
+//	printf("     **\n     **\n************\n************\n    *  *    \n    *  *    \n");
+//	return 0; 
+// }
+// 
+// 
+//int main()
+//{
+//	printf("Name    Age    Gender\n-----------------\nJack    18     man");
+//	return 0;
+//}
