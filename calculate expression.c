404139f9089e88110c65a725b8e103b20a8f093a
yq609 <1582746393@qq.com>
2024-10-06 15:50:55 +0800

    Delete calculate expression.c

--- calculate expression.c
+++
@@ -1,9 +0,0 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>
-////int main()
-////{
-////	int a = 40, c = 212;
-////	int r = (-8 + 22) * a - 10 + c / 2;
-////	printf("%d\n", r);
-////	return 0;
-////}
